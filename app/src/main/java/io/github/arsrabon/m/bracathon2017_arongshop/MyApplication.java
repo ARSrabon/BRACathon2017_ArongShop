035b33c625b75be2a5ea925b602f13bf13b9c24b
ARSrabon <m.arsrabon@gmail.com>
2017-02-25 23:22:00 +0600

    added google map api

--- app/src/main/java/io/github/arsrabon/m/bracathon2017_arongshop/MyApplication.java
+++ app/src/main/java/io/github/arsrabon/m/bracathon2017_arongshop/MyApplication.java
@@ -1,7 +1,8 @@
 package io.github.arsrabon.m.bracathon2017_arongshop;
 
 import android.app.Application;
-
+import android.content.Context;
+import android.support.multidex.MultiDex;
 /**
  * Created by msrabon on 2/18/17.
  */
@@ -10,5 +11,13 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        MultiDex.install(this);
     }
+
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+        MultiDex.install(this);
+    }
+
 }
--- app/src/main/java/io/github/arsrabon/m/bracathon2017_arongshop/activity/MenuActivity.java
+++ app/src/main/java/io/github/arsrabon/m/bracathon2017_arongshop/activity/MenuActivity.java
@@ -1,16 +1,30 @@
 package io.github.arsrabon.m.bracathon2017_arongshop.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import io.github.arsrabon.m.bracathon2017_arongshop.R;
 
 public class MenuActivity extends AppCompatActivity {
+
+    Button btn_todaysRoute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        btn_todaysRoute = (Button) findViewById(R.id.btn_todayRouteMap);
+
+        btn_todaysRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this,ShowOutletsOnMap_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
